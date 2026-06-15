13aafbb242e04e000d6ce4c2655a60eb28776429
malaika-zafar123 <malaikazafar236@gmail.com>
2026-06-14 22:40:04 -0700

    add

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    base: '/kanbannpm install gh-pages --save-dev/',
+    base: '/kanban/',
 })
